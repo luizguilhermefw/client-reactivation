d875ee1183b16584a988bd72c9aa0227364dd4b9
luiz.guilherme <ziulprof@gmail.com>
2026-06-12 23:29:51 -0300

    fix: full cors + cleanup dotenv

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
   });
 
   app.useGlobalPipes(
@@ -21,4 +21,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
