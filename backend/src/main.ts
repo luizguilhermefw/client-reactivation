eb959ee97e84b53c51e3151a78db639ea3e13587
luiz.guilherme <ziulprof@gmail.com>
2026-06-16 22:04:14 -0300

    fix: cors definitivo

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as cors from 'cors'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
-  app.enableCors({
+  app.use(cors({
     origin: true,
     credentials: true,
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
-  });
+  }))
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,8 +17,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
       transform: true,
     }),
-  );
+  )
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
