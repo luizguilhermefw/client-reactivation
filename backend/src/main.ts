d32136023a0cfcfa2dcc6934cfffc66e9da319a2
luiz.guilherme <ziulprof@gmail.com>
2026-06-16 22:12:55 -0300

    fix: cors correto via enableCors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,16 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.use(cors({
+  app.enableCors({
     origin: true,
     credentials: true,
-  }))
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+  })
 
   app.useGlobalPipes(
     new ValidationPipe({
