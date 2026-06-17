efe5d082445c88428115a08996ce548787fa81c3
luiz.guilherme <ziulprof@gmail.com>
2026-06-16 22:21:46 -0300

    fix: handle options preflight manual

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,25 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Request, Response } from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  // 🔥 FIX DEFINITIVO OPTIONS
+  app.use((req: Request, res: Response, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS')
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+      return res.sendStatus(204)
+    }
+    next()
+  })
+
   app.enableCors({
     origin: true,
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: '*',
   })
 
   app.useGlobalPipes(
