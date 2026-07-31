8692cd76aaf72972386fd323903f647dba73dbd5
luiz.guilherme <ziulprof@gmail.com>
2026-07-31 15:24:33 -0300

    feat(queue): deliver outbound messages through provider

--- backend/src/queue/queue.module.ts
+++ backend/src/queue/queue.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MessageProviderModule } from '../message-provider/message-provider.module';
 import { MessageWorkerService } from './message-worker.service';
 import { QueueService } from './queue.service';
 
 @Module({
+  imports: [MessageProviderModule],
   providers: [QueueService, MessageWorkerService],
   exports: [QueueService],
 })
